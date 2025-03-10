ab2e5ba3a9a3c5737c28daf50eba0fac116f9ace
Narumi <lia.lycka@gmail.com>
2025-03-10 14:38:06 +0900

    例外エラーをglobalexceptionHandlerでキャッチ、ErrorControllerでVIEWを返却するように修正

--- DroneInventorySystem/src/main/java/com/digitalojt/web/controller/ErrorController.java
+++ DroneInventorySystem/src/main/java/com/digitalojt/web/controller/ErrorController.java
@@ -1,11 +1,13 @@
 package com.digitalojt.web.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.autoconfigure.web.servlet.error.AbstractErrorController;
+import org.springframework.boot.web.servlet.error.ErrorAttributes;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ModelAttribute;
 
-import com.digitalojt.web.consts.ModelAttributeContents;
 import com.digitalojt.web.consts.UrlConsts;
 
 /**
@@ -14,13 +16,26 @@ import com.digitalojt.web.consts.UrlConsts;
  * @author dotlife
  */
 @Controller
-public class ErrorController {
+public class ErrorController extends AbstractErrorController {
 
+    private static final Logger logger = LoggerFactory.getLogger(ErrorController.class);
+
+    public ErrorController(ErrorAttributes errorAttributes) {
+        super(errorAttributes);
+    }
+
+    /**
+     * エラーコントローラー
+     * FlashAttribute で渡されたエラーメッセージを受け取りログ出力する。
+     * 
+     * @param errorMsg GlobalExceptionHandler から渡されたエラーメッセージ
+     * @return エラーページ（error.html）
+     */
 	@GetMapping(UrlConsts.ERROR)
-	public String handleError(Model model, @RequestParam(required = false) String errorMsg) {
-		if (errorMsg != null) {
-			model.addAttribute(ModelAttributeContents.ERROR_MSG, errorMsg); // エラーメッセージをモデルに追加
-		}
+    public String handleError(@ModelAttribute("errorMsg") String errorMsg) {  
+
+		logger.error("エラー内容: {}", errorMsg);
+		
 		return UrlConsts.ERROR_VIEW;
 	}
 }
